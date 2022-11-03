d67642b351c30372f91fd98a40fca0299d211167
Khris Bharmmano <84142253+khris-cpu@users.noreply.github.com>
2022-11-03 19:46:33 +0700

    add : JavaScript Comment

--- JavaScript-Fundamentals-Course/script.js
+++ JavaScript-Fundamentals-Course/script.js
@@ -3,4 +3,11 @@ console.log("Hello World!");
 document.getElementById("text").innerHTML = "Khris Bharmmano";
 document.write("Hello Document Write");
 
-window.alert("Hello Alert!")
+window.alert("Hello Alert!");
+
+// First Comment
+
+
+/* 
+    Many Comments
+*/
