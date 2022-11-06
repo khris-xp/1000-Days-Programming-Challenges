cde2bc5576d6785c0f7e316956f9e2a621c81624
Khris Bharmmano <84142253+khris-cpu@users.noreply.github.com>
2022-11-06 16:27:01 +0700

    edit : main.js

--- Learn-TypeScript-Full-Course-for-Beginners/Any-Void-Never-Unknown/dist/main.js
+++ Learn-TypeScript-Full-Course-for-Beginners/Any-Void-Never-Unknown/dist/main.js
@@ -1,7 +1,22 @@
-var popularTag = ['Dragon', 'Coffee'];
-var dragonsTag = [];
-var username = "Khris";
-var pageName = "1";
-var errorMessage = null;
-var user = null;
-var SomeProp;
+var doSomeThing = function () {
+    console.log("Do Something!");
+};
+var mFunction = function () {
+    throw "Nerer Gonna Give UP!";
+    // console.log("Never Gonna Give UP!");
+};
+{ /*
+    void -> undefined
+    any -> Any Types
+*/
+}
+var foo = undefined;
+// foo = 1; : Error
+var test = "1";
+test = 5;
+var vAny = 10;
+var vUnknown = 10;
+var s1 = vAny;
+var s2 = vUnknown;
+var pageNumber = '1';
+// let numericPageNumber: number = pageNumber as number; ## Error
