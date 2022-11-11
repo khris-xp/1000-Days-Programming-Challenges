ce6e8de3b7d1a01fd44370b03abb766dcfe7dae2
Khris Bharmmano <84142253+khris-cpu@users.noreply.github.com>
2022-11-11 22:58:45 +0700

    add : typescript types vs javascript types

---
+++ Understanding-TypeScript - 2023-Edition/TypeScripts-Basics-&-Basic-Types/TypeScript-Types-vs-JavaScript-Types/app.ts
@@ -0,0 +1,12 @@
+const add = (n1: number, n2: number) => {
+    // if (typeof n1 === 'number' && typeof n2 === 'number') {
+    //     throw new Error('Incorrect Input!');
+    // }
+    return n1 + n2
+}
+
+const number1 = 11;
+const number2 = 5;
+
+const result = add(number1, number2);
+console.log(result)
