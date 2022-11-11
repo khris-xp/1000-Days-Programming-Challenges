61f66f11ef52a0494c2c423ce2acad920a5b5ad4
Khris Bharmmano <84142253+khris-cpu@users.noreply.github.com>
2022-11-12 01:05:59 +0700

    add : java tutorial for beginners

---
+++ Java-Tutorial-Full-Course-for-Beginners/Java-tutorial-for-beginners/Java-tutorial-for-beginners/main.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+class main {
+    public static void main(String[] args) {
+
+        // Println
+
+        System.out.println("Hello World!");
+        System.out.println("I am a Java Developer");
+
+        /*
+         * Print
+         * System.out.print("I am a Java Developer");
+         * System.out.print("Hello World!");
+         */
+    }
+}
