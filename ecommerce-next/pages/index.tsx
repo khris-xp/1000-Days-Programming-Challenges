f6b5e950cac47d396667d8427e059e434f3db3f3
Khris Bharmmano <84142253+khris-cpu@users.noreply.github.com>
2022-11-13 12:40:23 +0700

    typescript config

--- ecommerce-next/pages/index.tsx
+++ ecommerce-next/pages/index.tsx
@@ -2,10 +2,17 @@ export default function Home() {
 
   const message: string = "Hello World"
 
-  let age: number = 18
+  let a: AddEventListenerOptions;
+  let b: NodeJS.Process
 
+  let person: Person = {
+    name: "Khris"
+  }
+
+  console.log(person.name)
 
   return (
+
     <div>{message}</div>
   )
 }
