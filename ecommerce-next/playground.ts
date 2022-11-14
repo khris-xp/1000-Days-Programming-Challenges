23ce3833a9400ea811f8043565cfadff24c50b45
Khris Bharmmano <84142253+khris-cpu@users.noreply.github.com>
2022-11-14 17:58:59 +0700

    add : extending type

--- ecommerce-next/playground.ts
+++ ecommerce-next/playground.ts
@@ -11,6 +11,16 @@ interface AcademicPerson extends Person {
     publications: string[]
 }
 
+// Type
+type Car = {
+    name: string
+}
+
+type RaceCar = {
+    speed: number
+} & AcademicPerson & { mileage: number }
+
+
 export default function Play() {
     const person: BusinessPerson = {
         name: "Khris",
@@ -19,14 +29,27 @@ export default function Play() {
     }
 
     const person2: AcademicPerson = {
-        name : "John",
+        name: "John",
         age: 34,
-        publications: ["1","2","3"]
+        publications: ["1", "2", "3"]
+    }
+
+    const myCar: RaceCar = {
+        name: "My Car",
+        speed: 100,
+        mileage: 200,
+        publications: [""],
+        age: 20
     }
 
     const logPerson = (person: Person) => {
         console.log(person)
     }
+
+    const logCar = (myCar: Car) => {
+        console.log(myCar);
+    }
+
     logPerson(person);
-    logPerson(person2)
+    logCar(myCar);
 }
