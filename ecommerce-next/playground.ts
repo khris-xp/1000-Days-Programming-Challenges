02686b1f1191de3ba5e17f6f35738bb62c3d8ce0
Khris Bharmmano <84142253+khris-cpu@users.noreply.github.com>
2022-11-14 15:05:52 +0700

    add : narrowing

--- ecommerce-next/playground.ts
+++ ecommerce-next/playground.ts
@@ -1,54 +1,13 @@
-// interface Person {
-//     name: string
-//     age: number
-// }
+export default function Play() {
 
-class Person {
-    name: string
-    age: number
+    const random = Math.random() > 0.5 ? "hello" : [1, 2]
 
-    constructor(name: string, age: number) {
-        this.name = name;
-        this.age = age;
+    if (typeof random === 'string') {
+        const upper = random.toUpperCase()
     }
+    else {
+        console.log(random);
+    }
+
+    console.log(random);
 }
-
-// Type
-type logPersonInfoFun = (name: string, age: number) => string
-
-// type logPersonInfoFun = (name: string, age: number) => string --> Optionsal Paramerters ( Age Can be use or not )
-
-// interface logPersonInfoFun {
-//     (name: string, age: number): string
-// }
-
-// Type Allias
-type Data = string;
-
-
-const Play = () => {
-
-    const name: Data = "Khris Bharmmano";
-    const age: number = 18;
-
-    const logPersonInfo: logPersonInfoFun = (personName: string, personAge: number ): string => {
-        return `Name : ${personName} , Age : ${personAge}`
-    }
-    console.log(logPersonInfo(name, age));
-
-    // Class
-
-    // const person: Person = {
-    //     name: "John",
-    //     age: 34
-    // }
-
-    const logPersonInfo2 = (person: Person) => {
-        const info = `Name : ${person.name} , Age: ${person.age}`
-        return info
-    }
-    console.log(logPersonInfo2(new Person("Michale", 22)));
-
-}
-
-export default Play;
