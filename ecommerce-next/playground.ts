801c0fd1f59d9fa72abac5b4f79f0fbe835bff7c
Khris Bharmmano <84142253+khris-cpu@users.noreply.github.com>
2022-11-15 19:47:02 +0700

    add : unions

--- ecommerce-next/playground.ts
+++ ecommerce-next/playground.ts
@@ -1,55 +1,18 @@
-interface Person {
-    name: string
-    age: number
-}
-
-interface BusinessPerson extends Person {
-    salary: number
-}
-
-interface AcademicPerson extends Person {
-    publications: string[]
-}
-
-// Type
-type Car = {
-    name: string
-}
-
 type RaceCar = {
-    speed: number
-} & AcademicPerson & { mileage: number }
+    name: string
+    maxSpeed: number
+}
+
+type CityCar = {
+    name: string
+    space: string
+}
 
 
 export default function Play() {
-    const person: BusinessPerson = {
-        name: "Khris",
-        age: 18,
-        salary: 14000
+
+    const logCarInfo = (car: RaceCar | CityCar) => {
+        console.log(car.name);
     }
 
-    const person2: AcademicPerson = {
-        name: "John",
-        age: 34,
-        publications: ["1", "2", "3"]
-    }
-
-    const myCar: RaceCar = {
-        name: "My Car",
-        speed: 100,
-        mileage: 200,
-        publications: [""],
-        age: 20
-    }
-
-    const logPerson = (person: Person) => {
-        console.log(person)
-    }
-
-    const logCar = (myCar: Car) => {
-        console.log(myCar);
-    }
-
-    logPerson(person);
-    logCar(myCar);
 }
