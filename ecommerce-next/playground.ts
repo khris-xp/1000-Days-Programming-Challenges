cf651ba2a63196e79141c186d18c4ea16acd3609
Khris Bharmmano <84142253+khris-cpu@users.noreply.github.com>
2022-11-17 12:08:38 +0700

    add : custom generic type

--- ecommerce-next/playground.ts
+++ ecommerce-next/playground.ts
@@ -1,24 +1,34 @@
-
 interface Person {
-    kind: "business" | "academic" | "others"
     name: string
     age: number
 }
 
-interface BusinessPerson extends Person {
-    kind: "business"
-    salary: number
-}
+class Logger<T> {
 
-interface AcademicPerson extends Person {
-    kind: "academic"
-    publications: string[]
+    log(items: Array<T>, callback: (i: T) => void) {
+        items.forEach((item) => {
+            callback(item)
+        })
+    }
 }
 
 export default function Play() {
-    const iterate = (items: Array<string>) => {
-        return items.forEach((items) => console.log(items.toUpperCase()))
-    }
 
-    iterate(["johns", "khris", "roger"])
+    const logger = new Logger<string>
+    const testArr = ["test1", "test2", "test3"];
+    logger.log(testArr, (item => {
+        console.log(item)
+    }))
+
+    const logger2 = new Logger<number>
+    const numArr = [1, 2, 3]
+    logger2.log(numArr, (item => {
+        console.log(item)
+    }))
+
+    const logger3 = new Logger<Person>
+    const personArr = [{ name: 'khris', age: 18 }, { name: 'John', age: 34 }, { name: 'Paul', age: 42 }]
+    logger3.log(personArr, (item => {
+        console.log(item)
+    }))
 }
