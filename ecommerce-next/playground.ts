3fb21d1c6a6c006ce40bba5ef9750668071f5f0b
Khris Bharmmano <84142253+khris-cpu@users.noreply.github.com>
2022-11-19 12:05:11 +0700

    add : array index types

--- ecommerce-next/playground.ts
+++ ecommerce-next/playground.ts
@@ -2,37 +2,12 @@ interface Person {
     name: string
 }
 
-interface Student extends Person {
-    name: string
-    age: number
-}
-
-interface PostGraduateStudent extends Person {
-    project?: string[]
-    age: number
-}
-
-type StudentInfo<T extends any = Student> = T extends Student ? {
-    data: T
-    grades: number[]
-} : string
-
-type Car = { engine: string }
+type SingleType<T> = T extends any[] ? T[number] : T
 
 export default function Play() {
+    type Type1 = SingleType<string[]>
+    type Type2 = SingleType<number[]>
+    type Type3 = SingleType<Person>
 
-    const logStudentInfo = (info: StudentInfo<Student>) => {
-        console.log(info)
-    }
-
-    const info = {
-        data: {
-            name: "Khris",
-            age: 18,
-        },
-        grades: [10, 20],
-        project: ["Project1", "Project2"]
-    }
-
-    logStudentInfo(info);
+    type Type4 = string[][number]
 }
