739aa1b376fc02e8c3528b8000ae41bc3d6b1684
Khris Bharmmano <84142253+khris-cpu@users.noreply.github.com>
2022-11-19 11:45:09 +0700

    add : generic types with extends

--- ecommerce-next/playground.ts
+++ ecommerce-next/playground.ts
@@ -1,44 +1,36 @@
 interface Person {
     name: string
-    age: number
-    city: string
 }
 
-interface Car {
-    brand: string
+interface Student extends Person {
     name: string
-    city: string
     age: number
 }
 
-class BusinessPerson implements Person {
-    name = ""
-    age = 0
-    city = ""
-    salary?= 1000
+interface PostGraduateStudent extends Person {
+    project?: string[]
+    age: number
 }
 
-class Student implements Person {
-    name = ""
-    age = 0
-    city = ""
-}
-
-class Logger<T extends Person = Person> {
-
-    log(items: Array<T>, callback: (i: T) => void) {
-        items.forEach((item) => {
-            callback(item)
-        })
-    }
+interface StudentInfo<T extends Student = Student> {
+    data: T
+    grades: number[]
 }
 
 export default function Play() {
 
-    const logger = new Logger()
-    //const personArr = [{ name: 'Khris', age: 18, city: "Bangkok", salary: 1000 }, { name: 'John', age: 34, city: "New York" }, { name: 'Paul', age: 42, city: "New York" }]
-    const carArr = [{ name: 'Ford V1', age: 2, city: 'Bangkok', brand: 'Ford Mustang' }, { name: 'Ford V2', age: 3, city: 'Bangkok', brand: 'Ford Mustang' }, { name: 'Toyota V1', age: 4, city: 'New York', brand: 'Toyota' }]
-    logger.log(carArr, (item => {
-        console.log(item)
-    }))
+    const logStudentInfo = (info: StudentInfo<PostGraduateStudent>) => {
+        console.log(info)
+    }
+
+    const info = {
+        data: {
+            name: "Khris",
+            age: 18,
+        },
+        grades: [10, 20],
+        project: ["Project1", "Project2"]
+    }
+
+    logStudentInfo(info);
 }
