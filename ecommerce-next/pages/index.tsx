53a6bd498ab7c15597d289c995a0a804021fb25f
Khris Bharmmano <84142253+khris-cpu@users.noreply.github.com>
2022-11-19 12:12:35 +0700

    add : infer get static props

--- ecommerce-next/pages/index.tsx
+++ ecommerce-next/pages/index.tsx
@@ -1,14 +1,25 @@
-import Play from "../playground"
 import { useEffect } from "react"
+import type { InferGetStaticPropsType } from "next"
 
-export default function Home() {
-  useEffect(() => {
-    Play();
-  }, [])
+export async function getStaticProps() {
+
+  const products = [1, 2, 3]
+
+  return {
+    props: {
+      products
+    },
+    revalidate: 4 * 60 * 60
+  }
+}
+
+export default function Home({ products }: InferGetStaticPropsType<typeof getStaticProps>) {
 
   return (
 
-    <div>Hello World!</div>
+    <div>
+      {products}
+    </div>
 
   )
 }
