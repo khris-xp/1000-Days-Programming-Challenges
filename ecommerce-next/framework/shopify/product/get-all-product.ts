80c4c62c3ec059a8af7f935b47895403608dda06
Khris Bharmmano <84142253+khris-cpu@users.noreply.github.com>
2022-11-24 19:26:31 +0700

    add : map product from connection type

--- ecommerce-next/framework/shopify/product/get-all-product.ts
+++ ecommerce-next/framework/shopify/product/get-all-product.ts
@@ -3,17 +3,17 @@ import fetchAPI from "../utils/fetch-api";
 
 import { ProductConnection } from "../schema";
 
-type FetchParams = {
-    query: string
-}
-
 type ReturnType = {
     products: ProductConnection
 }
 
 const getAllProducts = async (): Promise<any> => {
-    const { data } = await fetchAPI<{ products: ProductConnection }>({ query: getAllProductQuery })
-    return data.products
+    const { data } = await fetchAPI<ReturnType>({ query: getAllProductQuery })
+
+    const products = data.products.edges.map(({ node: product }) => {
+        return product
+    }) ?? []
+    return products
 }
 
 export default getAllProducts
