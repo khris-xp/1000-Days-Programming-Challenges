13ed07416e9743a23b6a25119023c7551a813d89
Khris Bharmmano <84142253+khris-cpu@users.noreply.github.com>
2022-11-27 20:13:04 +0700

    add : A + B Problem

---
+++ Dev-Lab-Java/ABProblem.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class ABProblem {
+    public static void main(String[] args) {
+        Scanner myInput = new Scanner(System.in);
+
+        int A = myInput.nextInt();
+        int B = myInput.nextInt();
+        System.out.println(A + B);
+    }
+}
