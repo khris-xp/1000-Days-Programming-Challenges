57b74cf3b42ab023f38e710572573a30ba86b49a
Khris Bharmmano <84142253+khris-cpu@users.noreply.github.com>
2022-11-27 20:28:21 +0700

    add : Loop

---
+++ Dev-Lab-Java/Loop.java
@@ -0,0 +1,12 @@
+import java.util.*;;
+
+public class Loop {
+    public static void main(String[] args) {
+        Scanner myInput = new Scanner(System.in);
+
+        int round = myInput.nextInt();
+        for(int i = 0 ; i < round; i++){
+            System.out.println("A");
+        }
+    }
+}
