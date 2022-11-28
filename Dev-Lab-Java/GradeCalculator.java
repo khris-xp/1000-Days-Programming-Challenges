05325cfb842f7a040444c057390232c0c8eabfad
Khris Bharmmano <84142253+khris-cpu@users.noreply.github.com>
2022-11-28 21:57:55 +0700

    add : Grade Calculator

---
+++ Dev-Lab-Java/GradeCalculator.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class GradeCalculator {
+    public static void main(String[] args) {
+        Scanner myInput = new Scanner(System.in);
+
+        int score = myInput.nextInt();
+        if (score >= 80 && score < 100) {
+            System.out.println("A");
+        } else if (score >= 70 && score <= 79) {
+            System.out.println("B");
+        } else if (score >= 60 && score <= 69) {
+            System.out.println("C");
+        } else if (score >= 50 && score <= 59) {
+            System.out.println("D");
+        } else if (score < 50) {
+            System.out.println("F");
+        }
+    }
+}
