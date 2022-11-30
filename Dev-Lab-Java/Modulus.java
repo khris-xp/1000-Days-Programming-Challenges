779659011ff4d95988ffde6ff9bb909d23c5822c
Khris Bharmmano <84142253+khris-cpu@users.noreply.github.com>
2022-11-30 22:31:14 +0700

    add : modulus

---
+++ Dev-Lab-Java/Modulus.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Modulus {
+    public static void main(String[] args) {
+        Scanner myInput = new Scanner(System.in);
+
+        int a = myInput.nextInt();
+        int b = myInput.nextInt();
+
+        System.out.println(a % b);
+    }
+}
