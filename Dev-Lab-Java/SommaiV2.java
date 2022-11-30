61768145d621022d57ff0cd68db506ae91b39af5
Khris Bharmmano <84142253+khris-cpu@users.noreply.github.com>
2022-11-30 22:34:40 +0700

    add : Sommai V2.0

---
+++ Dev-Lab-Java/SommaiV2.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class SommaiV2 {
+    public static void main(String[] args) {
+      Scanner myInput = new Scanner(System.in);
+      
+      String name = myInput.nextLine();
+
+      System.out.println("Welcome " + name + " !");
+      System.out.println("Sommai 108 Eleven Shop");
+    }
+}
