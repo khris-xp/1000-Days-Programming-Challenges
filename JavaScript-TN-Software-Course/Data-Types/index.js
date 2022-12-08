2101e418bb5e24ca19f4c2964e6ebaf17a14133a
Khris Bharmmano <84142253+khris-cpu@users.noreply.github.com>
2022-12-08 17:57:09 +0700

    add : Data Types

---
+++ JavaScript-TN-Software-Course/Data-Types/index.js
@@ -0,0 +1,36 @@
+let x = 10;
+let y = 5.5;
+let firstName = "Khris";
+let boolean = true;
+
+/*
+console.log(x);
+console.log(y);
+console.log(firstName);
+console.log(boolean);
+*/
+
+console.log(typeof (x));
+console.log(typeof (y));
+console.log(typeof (firstName));
+console.log(typeof (boolean));
+
+/*
+
+let num1 = "11";
+let num2 = "12";
+
+let num1 = "11";
+let num2 = 12;
+
+*/
+
+let num1 = 11;
+let num2 = 12;
+
+console.log(num1 + num2);
+
+let num3 = Number("15");
+let num4 = 11;
+
+console.log(num3 + num4);
