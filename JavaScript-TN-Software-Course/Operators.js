c6c64433e70e621686a63f70283148a43ec933bc
Khris Bharmmano <84142253+khris-cpu@users.noreply.github.com>
2022-12-08 18:16:52 +0700

    add : Operators

---
+++ JavaScript-TN-Software-Course/Operators.js
@@ -0,0 +1,39 @@
+let num1 = 3 + 2;
+let num2 = 3 - 2;
+let num3 = 3 * 2;
+let num4 = 3 / 2;
+let num5 = 3 ** 2;
+let num6 = 3 % 2;
+
+let x = 5;
+// x++
+x += 1;
+console.log(x);
+//x--;
+x -= 1;
+console.log(x);
+
+x = 7;
+// let y = 5;
+let y = 7;
+
+console.log(x < y);
+console.log(x <= y);
+
+console.log(x > y);
+console.log(x >= y);
+console.log(x < y);
+console.log(x <= y);
+
+console.log(x === y);
+console.log(x !== y);
+
+let p = true;
+let q = false;
+
+console.log(p || q);
+console.log(p && q);
+
+console.log(!p);
+
+// Example --> !p || q
