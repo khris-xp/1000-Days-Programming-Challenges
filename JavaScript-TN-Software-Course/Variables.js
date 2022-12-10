d0737fa0e83b71a50a279f9d6e0ba255c56a7c59
Khris Bharmmano <84142253+khris-cpu@users.noreply.github.com>
2022-12-10 15:04:16 +0700

    updates : Variables

--- JavaScript-TN-Software-Course/Variables.js
+++ JavaScript-TN-Software-Course/Variables.js
@@ -1,13 +1,23 @@
+// Example 1
 let x = 2;
 console.log(x);
 
+// Example 2
 x = 10;
 console.log(x);
 
+// Example 3
 let a = 1;
 let b = 2;
 let c = a + b;
-
 console.log(c);
 
+// Example 4
 let username = "Khris";
+
+/*
+    Output 1 = 2
+    Output 2 = 10
+    Output 3 = 3
+    Output 4 = Khris
+*/
