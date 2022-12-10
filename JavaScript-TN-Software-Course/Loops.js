db4aef56a5c8c04b1c471982e5a762ec061766d6
Khris Bharmmano <84142253+khris-cpu@users.noreply.github.com>
2022-12-10 15:39:25 +0700

    updates : Loops

--- JavaScript-TN-Software-Course/Loops.js
+++ JavaScript-TN-Software-Course/Loops.js
@@ -1,14 +1,15 @@
-/*
+// Example 1
 for (let i = 1; i <= 10; i++) {
     console.log(i);
 }
-*/
 
+// Example 2
 for (let i = 1; i <= 5; i++) {
     let answer = i ** 2;
     console.log(i + " ** 2 : " + answer);
 }
 
+// Example 3
 for (let i = 1; i <= 10; i++) {
     if (i % 2 == 0) {
         continue;
@@ -17,6 +18,7 @@ for (let i = 1; i <= 10; i++) {
     console.log(i + " ** 2 : " + answer);
 }
 
+// Example 4
 for (let i = 1; i <= 13; i++) {
     if (i % 10 == 0) {
         break;
@@ -24,3 +26,10 @@ for (let i = 1; i <= 13; i++) {
     let answer = i ** 2;
     console.log(i + " ** 2 : " + answer);
 }
+
+/* 
+    Output 1 = 1 - 10
+    Output 2 = (1 ** 2) - (5 ** 2)
+    Output 3 = (1 ** 2), (3 ** 2), (5 ** 2), (7 ** 2), (9 ** 2)
+    Output 4 = (1 ** 2) - (9 ** 2)  
+*/
