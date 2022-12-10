54442450f2363eb073a2773123d29880afe8642a
Khris Bharmmano <84142253+khris-cpu@users.noreply.github.com>
2022-12-10 15:34:44 +0700

    updates : Operators

--- JavaScript-TN-Software-Course/Operators.js
+++ JavaScript-TN-Software-Course/Operators.js
@@ -1,18 +1,45 @@
+// Example 1
 let num1 = 3 + 2;
+console.log(num1);
+
+// Example 2
 let num2 = 3 - 2;
+console.log(num2);
+
+// Example 3
 let num3 = 3 * 2;
+console.log(num3);
+
+// Example 4
 let num4 = 3 / 2;
+console.log(num4);
+
+// Example 5
 let num5 = 3 ** 2;
+console.log(num5);
+
+// Example 6
 let num6 = 3 % 2;
+console.log(num6);
 
-let x = 5;
-// x++
-x += 1;
-console.log(x);
-//x--;
-x -= 1;
-console.log(x);
+// Example 7
+let x1 = 5;
+console.log(x1++);
 
+// Example 8
+let x2 = 5;
+x2 += 1;
+console.log(x2);
+
+// Example 9
+let x3 = 5;
+console.log(x3--);
+
+// Example 10
+let x4 = 5;
+console.log(x4 -= 1);
+
+// Example 11
 x = 7;
 // let y = 5;
 let y = 7;
@@ -28,6 +55,7 @@ console.log(x <= y);
 console.log(x === y);
 console.log(x !== y);
 
+// Example 12
 let p = true;
 let q = false;
 
@@ -36,4 +64,5 @@ console.log(p && q);
 
 console.log(!p);
 
-// Example --> !p || q
+// Exercise
+console.log(!p || q);
