9b6205ed7e2b40a38af32a6f3f6594d01c9353ca
Khris Bharmmano <84142253+khris-cpu@users.noreply.github.com>
2022-12-10 15:13:34 +0700

    updates : Data Types

--- JavaScript-TN-Software-Course/Data-Types.js
+++ JavaScript-TN-Software-Course/Data-Types.js
@@ -1,36 +1,58 @@
+// Example 1
 let x = 10;
-let y = 5.5;
-let firstName = "Khris";
-let boolean = true;
-
-/*
-console.log(x);
-console.log(y);
-console.log(firstName);
-console.log(boolean);
-*/
-
 console.log(typeof (x));
+
+// Example 2
+
+let y = 5.5;
 console.log(typeof (y));
+
+// Example 3
+let firstName = "Khris";
 console.log(typeof (firstName));
+
+// Example 4
+let boolean = true;
 console.log(typeof (boolean));
 
-/*
-
+// Example 5
 let num1 = "11";
 let num2 = "12";
 
-let num1 = "11";
-let num2 = 12;
-
-*/
-
-let num1 = 11;
-let num2 = 12;
-
 console.log(num1 + num2);
 
-let num3 = Number("15");
-let num4 = 11;
+// Example 6
+let num3 = "11";
+let num4 = 12;
 
 console.log(num3 + num4);
+
+// Example 7
+let num5 = 11;
+let num6 = 12;
+
+console.log(num5 + num6);
+
+// Example 8
+let num7 = Number("11");
+let num8 = 15;
+
+console.log(num7 + num8);
+
+// Example 9 
+let num9 = String("23");
+let num10 = "12";
+
+console.log(num9 + num10);
+
+/* 
+    Output 1 = Number
+    Output 2 = Number
+    Output 3 = String
+    Output 4 = Boolean
+    Output 5 = 1112
+    Output 6 = 1112
+    Output 7 = 23
+    Output 8 = 26
+    Output 9 = 2312
+*/
