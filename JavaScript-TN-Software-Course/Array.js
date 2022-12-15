9670fdff4e52981bce679f08868e047442b069c5
Khris Bharmmano <84142253+khris-cpu@users.noreply.github.com>
2022-12-15 21:55:56 +0700

    add : Array

---
+++ JavaScript-TN-Software-Course/Array.js
@@ -0,0 +1,31 @@
+// Example 1
+let myArray = ['Banana', 'Watermelon', 'Carrot'];
+
+// Example 2
+myArray[1] = 'Melon'
+
+// Example 3
+let newArray = ['John', 'Jane', 'Jim'];
+
+// // Exampel 4
+newArray.push('Josh');
+
+// Example 5
+newArray.shift('Josh');
+
+// Example 6
+newArray.unshift('Kim');
+
+// Example 7
+newArray.pop('Jonathan');
+
+// Exercise // Output --> [3,1,6,5,4,2]
+
+let exArray = [1, 2, 3, 4]
+
+exArray.push(2);
+exArray.unshift(3);
+exArray[2] = 6
+exArray[3] = 5
+
+console.log(exArray);
