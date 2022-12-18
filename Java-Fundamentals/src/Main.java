407810b9b99630151f4dffc634b884bdd40fb895
Khris Bharmmano <84142253+khris-cpu@users.noreply.github.com>
2022-12-18 21:07:07 +0700

    add : Conditions

--- Java-Fundamentals/src/Main.java
+++ Java-Fundamentals/src/Main.java
@@ -1,14 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        double num1 = 15;
-        double num2 = 20;
+        Scanner myInput = new Scanner(System.in);
 
-        // System.out.println(num1 + num2);
-        // System.out.println(num1 - num2);
-        // System.out.println(num1 * num2);
-        // System.out.println(num1 / num2);
-        num1++;
-        // System.out.println(num1);
+        System.out.print("Your Price : ");
+        double firstPrice = myInput.nextDouble();
+        double finalPrice;
 
+        if (firstPrice > 10000) {
+            finalPrice = firstPrice - (0.15 * firstPrice);
+            System.out.println("Final Price : " + finalPrice);
+        } else if (firstPrice > 6000 && firstPrice < 10000) {
+            finalPrice = firstPrice - (0.1 * firstPrice);
+            System.out.println("Final Price : " + finalPrice);
+        } else if (firstPrice > 3000 && firstPrice < 6000) {
+            finalPrice = firstPrice - (0.08 * firstPrice);
+            System.out.println("Final Price : " + finalPrice);
+        } else {
+            finalPrice = firstPrice - (0.05 * firstPrice);
+            System.out.println("Final Price : " + finalPrice);
+        }
     }
 }
