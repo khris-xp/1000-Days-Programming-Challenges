65c1dabb7a40036702889b403b096a71850e32bd
Khris Bharmmano <84142253+khris-cpu@users.noreply.github.com>
2022-12-18 20:53:17 +0700

    add : Operators

--- Java-Fundamentals/src/Main.java
+++ Java-Fundamentals/src/Main.java
@@ -1,15 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        double latitude = 13.5555;
-        double longitude = 100.2222;
-        String str1 = "12";
-        int num1 = Integer.parseInt(str1);
+        double num1 = 15;
+        double num2 = 20;
 
-        int num2 = 12;
-        String str2 = String.valueOf(num2);
+        // System.out.println(num1 + num2);
+        // System.out.println(num1 - num2);
+        // System.out.println(num1 * num2);
+        // System.out.println(num1 / num2);
+        num1++;
+        // System.out.println(num1);
 
-//        System.out.println("( " + latitude + " , " + longitude + " )");
-        System.out.println(num1 + 5);
-        System.out.println(str2 + num2);
     }
 }
