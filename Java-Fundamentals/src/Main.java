160cc6cb8eacb9aeb6fc6dacd06138f4de551a70
Khris Bharmmano <84142253+khris-cpu@users.noreply.github.com>
2022-12-18 21:09:01 +0700

    add : Loops

--- Java-Fundamentals/src/Main.java
+++ Java-Fundamentals/src/Main.java
@@ -2,24 +2,8 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner myInput = new Scanner(System.in);
-
-        System.out.print("Your Price : ");
-        double firstPrice = myInput.nextDouble();
-        double finalPrice;
-
-        if (firstPrice > 10000) {
-            finalPrice = firstPrice - (0.15 * firstPrice);
-            System.out.println("Final Price : " + finalPrice);
-        } else if (firstPrice > 6000 && firstPrice < 10000) {
-            finalPrice = firstPrice - (0.1 * firstPrice);
-            System.out.println("Final Price : " + finalPrice);
-        } else if (firstPrice > 3000 && firstPrice < 6000) {
-            finalPrice = firstPrice - (0.08 * firstPrice);
-            System.out.println("Final Price : " + finalPrice);
-        } else {
-            finalPrice = firstPrice - (0.05 * firstPrice);
-            System.out.println("Final Price : " + finalPrice);
-        }
+       for(int i = 0 ; i < 5 ; i++){
+           System.out.println(i);
+       }
     }
 }
