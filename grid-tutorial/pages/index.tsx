e35c1e197a9b15f6ceb6621f8da1f7bfd212e930
Khris Bharmmano <84142253+khris-cpu@users.noreply.github.com>
2022-12-26 17:19:31 +0700

    add : layout with responsive

--- grid-tutorial/pages/index.tsx
+++ grid-tutorial/pages/index.tsx
@@ -10,21 +10,21 @@ import Items5 from "../public/images/items5.jpg";
 const Home: NextPage = () => {
   return (
     <div className="m-0 p-0 box-border">
-      <div className="grid grid-cols-[1fr_1fr_1fr_1fr] grid-rows-[300px_300px_300px] overflow-hidden font-Roboto">
+      <div className="grid lg:grid-cols-[1fr_1fr_1fr_1fr] lg:grid-rows-[300px_300px_300px] grid-cols-2 sm:grid-rows-[200px_300px_300px_300px_200px] grid-rows-[200px_200px_200px_200px_200px] overflow-hidden font-Roboto">
 
         {/* Items 1 */}
-        <div className="col-[1_/_3] row-[1_/_3]">
+        <div className="lg:col-[1_/_3] lg:row-[1_/_3] col-[1_/_5]">
           <Image src={Items4} height={1000} width={1000} alt="items1" className="w-full h-full object-cover" />
         </div>
 
         {/* Items 2 */}
-        <div className="grid grid-cols-[1fr_1fr] grid-rows-[300px] col-[3_/_5] row-[1_/_2]">
-          <div className="flex justify-center items-center p-[3rem]">
+        <div className="grid lg:grid-cols-[1fr_1fr] lg:grid-rows-[300px] lg:col-[3_/_5] lg:row-[1_/_2] grid-cols-[1fr_1fr] col-[1_/_5] row-[2_/_3] overflow-hidden">
+          <div className="flex justify-center items-center sm:p-[3rem] p-[1rem]">
             <div>
-              <h3 className="text-xl font-bold">Handmade Piece</h3>
-              <p className="text-xs mx-[0rem] my-[1rem]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi voluptates soluta quaerat laudantium incidunt eos eaque sunt odio iste ipsam ut eveniet debitis a excepturi eum, molestias dolorem ipsa unde.</p>
+              <h3 className="sm:text-xl font-bold min-[340]:text-base text-sm">Handmade Piece</h3>
+              <p className="sm:text-xs sm:mx-[0rem] sm:my-[1rem] text-[9px] my-[0.4rem]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi voluptates soluta quaerat laudantium incidunt eos eaque sunt odio iste ipsam ut eveniet debitis a excepturi eum, molestias dolorem ipsa unde.</p>
               <Link href="/">
-                <button className="inline-block bg-black text-white text-none px-[1rem] py-[0.5rem]">Check Out</button>
+                <button className="inline-block bg-black text-white text-none sm:px-[1rem] sm:py-[0.5rem] px-[0.5rem] py-[0.25rem]">Check Out</button>
               </Link>
             </div>
           </div>
@@ -34,39 +34,39 @@ const Home: NextPage = () => {
         </div>
 
         {/* Items 3 */}
-        <div className="grid grid-cols-[1fr_1fr] col-[3_/_5] row-[2/_3]">
+        <div className="grid lg:grid-cols-[1fr_1fr] lg:col-[3_/_5] lg:row-[2/_3] grid-cols-[1fr_1fr] col-[1_/_5] row-[3_/_4] overflow-hidden">
           <div>
             <Image src={Items3} height={500} width={500} alt="items3" className="w-full h-full object-cover" />
           </div>
-          <div className="flex justify-center items-center p-[3rem]">
+          <div className="flex justify-center items-center sm:p-[3rem] p-[1rem]">
             <div>
-              <h3 className="text-xl font-bold">Woodworking</h3>
-              <p className="text-xs mx-[0rem] my-[1rem]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi voluptates soluta quaerat laudantium incidunt eos eaque sunt odio iste ipsam ut eveniet debitis a excepturi eum, molestias dolorem ipsa unde.</p>
+              <h3 className="sm:text-xl font-bold min-[340]:text-base text-sm">Woodworking</h3>
+              <p className="sm:text-xs sm:mx-[0rem] sm:my-[1rem] text-[9px] my-[0.4rem]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi voluptates soluta quaerat laudantium incidunt eos eaque sunt odio iste ipsam ut eveniet debitis a excepturi eum, molestias dolorem ipsa unde.</p>
               <Link href="/">
-                <button className="inline-block bg-black text-white text-none px-[1rem] py-[0.5rem]">Check Out</button>
+                <button className="inline-block bg-black text-white text-none sm:px-[1rem] sm:py-[0.5rem] px-[0.5rem] py-[0.25rem]">Check Out</button>
               </Link>
             </div>
           </div>
         </div>
 
         {/* Items 4 */}
-        <div className="grid grid-cols-[1fr_1fr] col-[1_/_3] row-[3_/_4]">
+        <div className="grid lg:grid-cols-[1fr_1fr] lg:col-[1_/_3] lg:row-[3_/_4] grid-cols-[1fr_1fr] col-[1_/_5] row-[4_/_5]">
           <div>
             <Image src={Items1} height={500} width={500} alt="items4" className="w-full h-full object-cover" />
           </div>
-          <div className="flex justify-center items-center p-[3rem]">
+          <div className="flex justify-center items-center sm:p-[3rem] p-[1rem]">
             <div>
-              <h3 className="text-xl font-bold">Studio House</h3>
-              <p className="text-xs mx-[0rem] my-[1rem]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi voluptates soluta quaerat laudantium incidunt eos eaque sunt odio iste ipsam ut eveniet debitis a excepturi eum, molestias dolorem ipsa unde.</p>
+              <h3 className="sm:text-xl font-bold min-[340]:text-base text-sm">Studio House</h3>
+              <p className="sm:text-xs sm:mx-[0rem] sm:my-[1rem] text-[9px] my-[0.4rem]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi voluptates soluta quaerat laudantium incidunt eos eaque sunt odio iste ipsam ut eveniet debitis a excepturi eum, molestias dolorem ipsa unde.</p>
               <Link href="/">
-                <button className="inline-block bg-black text-white text-none px-[1rem] py-[0.5rem]">Check Out</button>
+                <button className="inline-block bg-black text-white text-none sm:px-[1rem] sm:py-[0.5rem] px-[0.5rem] py-[0.25rem]">Check Out</button>
               </Link>
             </div>
           </div>
         </div>
 
         {/* Items 5 */}
-        <div className="col-[3_/_5] row-[3_/_4]">
+        <div className="lg:col-[3_/_5] lg:row-[3_/_4] col-[1_/_5] row-[5_/_6]">
           <Image src={Items5} height={1000} width={1000} alt="items5" className="w-full h-full object-cover" />
         </div>
       </div>
