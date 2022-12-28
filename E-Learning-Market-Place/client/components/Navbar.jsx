6332f7087bc9717fb051c5e2370e4d2eca165d6e
Khris Bharmmano <84142253+khris-cpu@users.noreply.github.com>
2022-12-28 15:54:15 +0700

    add : ant menu and icons

--- E-Learning-Market-Place/client/components/Navbar.jsx
+++ E-Learning-Market-Place/client/components/Navbar.jsx
@@ -1,8 +1,21 @@
+import React, { Component } from 'react';
+import { Menu, Icon } from 'antd';
+import Link from 'next/link';
+import { HomeTwoTone, UserAddOutlined, LoginOutlined } from '@ant-design/icons';
+
 const Navbar = () => {
     return (
-        <div>
-            <h1>Navbar</h1>
-        </div>
+        <Menu mode='horizontal'>
+            <Menu.Item icon={<HomeTwoTone twoToneColor="#000" />}>
+                <Link href="/">Home</Link>
+            </Menu.Item>
+            <Menu.Item icon={<UserAddOutlined twoToneColor="#000" />}>
+                <Link href="/register">Register</Link>
+            </Menu.Item>
+            <Menu.Item icon={<LoginOutlined twoToneColor="#000" />}>
+                <Link href="/login">Login</Link>
+            </Menu.Item>
+        </Menu>
     )
 }
 
--- E-Learning-Market-Place/client/pages/index.jsx
+++ E-Learning-Market-Place/client/pages/index.jsx
@@ -8,7 +8,7 @@ const inter = Inter({ subsets: ['latin'] })
 const Home = () => {
   return (
     <div>
-      <h1 className='jumbotron square text-center'>Hello World!</h1>
+      <h1 className='jumbotron square text-center'>Online Education Marketplace</h1>
     </div>
   )
 }
