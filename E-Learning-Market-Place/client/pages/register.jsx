681dc7de5f5ddd43511374601b9e63e23cb41c6f
Khris Bharmmano <84142253+khris-cpu@users.noreply.github.com>
2022-12-28 16:24:00 +0700

    add : register page

--- E-Learning-Market-Place/client/pages/register.jsx
+++ E-Learning-Market-Place/client/pages/register.jsx
@@ -1,7 +1,30 @@
+import { useState } from "react";
+
 const Register = () => {
+
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.table({ name, email, password });
+    };
+
     return (
         <div>
             <h1 className='jumbotron text-center square'>Register</h1>
+            <div className="container col-md-4 offset-md-4 pb-5">
+                <form onSubmit={handleSubmit}>
+                    <input type="text" className="form-control mb-4 p-4" value={name} onChange={(e) => setName(e.target.value)} placeholder="Enter your name" required />
+                    <input type="email" className="form-control mb-4 p-4" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Enter your email" required />
+                    <input type="password" className="form-control mb-4 p-4" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Enter your password" required />
+                    <br />
+                    <button type="submit" className="btn btn-block btn-primary p-2">
+                        Submit
+                    </button>
+                </form>
+            </div>
         </div>
     )
 }
