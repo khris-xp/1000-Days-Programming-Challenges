620918793f75b6e6e5f1e6d64361484a043e0d20
Khris Bharmmano <84142253+khris-cpu@users.noreply.github.com>
2022-12-29 10:55:49 +0700

    add : express

--- E-Learning-Market-Place/server/server.js
+++ E-Learning-Market-Place/server/server.js
@@ -1,1 +1,27 @@
-console.log("Server is running");
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+
+require('dotenv').config();
+
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(morgan());
+app.use((req, res, next) => {
+    console.log("This is middleware function");
+    next();
+})
+
+// Route
+app.get('/', (req, res) => {
+    res.send(`Hello from Server`);
+})
+
+// PORT
+PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on PORT : ${PORT}`);
+});
