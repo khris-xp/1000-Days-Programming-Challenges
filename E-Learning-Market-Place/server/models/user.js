de3ced429582cfbf08ed28e16814f7a5612e4d7d
Khris Bharmmano <84142253+khris-cpu@users.noreply.github.com>
2022-12-29 14:51:14 +0700

    add : user model

---
+++ E-Learning-Market-Place/server/models/user.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    email: {
+        type: String,
+        trim: true,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        min: 6,
+        max: 64,
+    },
+    picture: {
+        type: String,
+        default: "/avatar.png",
+    },
+    role: {
+        type: [String],
+        default: ["Subscriber"],
+        enum: ["Subscriber", "Instructor", "Admin"],
+    },
+    stripe_acount_id: '',
+    strip_seller: {},
+    stripSession: {}
+}, {
+    timestamps: true
+});
+
+export default mongoose.model('User', userSchema);
