902341d5005a1c8f2a50f1bba638023d6211ee71
Khris Bharmmano <84142253+khris-cpu@users.noreply.github.com>
2022-12-31 23:19:20 +0700

    add : handling expired token using axios interceptors

--- E-Learning-Market-Place/client/context/index.jsx
+++ E-Learning-Market-Place/client/context/index.jsx
@@ -1,10 +1,13 @@
 import { useReducer, createContext, useEffect } from "react";
+import axios from "axios";
+import { useRouter } from "next/router";
 
 const initialState = {
     user: null
 }
 
 const Context = createContext();
+const router = useRouter();
 
 const rootReducer = (state, action) => {
     switch (action.type) {
@@ -26,6 +29,30 @@ const Provider = ({ children }) => {
             payload: JSON.parse(window.localStorage.getItem("user"))
         })
     }, [])
+
+    axios.interceptors.response.use(
+        function (response) {
+            return response;
+        },
+        function (error) {
+            const res = error.response;
+            if (res.status === "401" && res.config() && !res.config.__isRetryRequest) {
+                return new Promise((resolve, reject) => {
+                    axios.get('/api/logout')
+                        .then((data) => {
+                            console.log("/401 Error > logout.");
+                            dispatch({ type: "LOGOUT" });
+                            window.localStorage.removeItem("user");
+                            router.push();
+                        })
+                        .catch((err) => {
+                            console.log("AXIOS INTERCEPTOR ERR", err);
+                            reject(err);
+                        })
+                })
+            }
+        }
+    )
 
     return (
         <Context.Provider value={{ state, dispatch }}>{children}</Context.Provider>
