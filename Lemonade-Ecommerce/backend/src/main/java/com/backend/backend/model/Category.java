795bac78fefd84c23a2dea0b898f22a2b65575af
Khris Bharmmano <84142253+khris-cpu@users.noreply.github.com>
2023-01-04 12:22:30 +0700

    add : category model

---
+++ Lemonade-Ecommerce/backend/src/main/java/com/backend/backend/model/Category.java
@@ -0,0 +1,59 @@
+package com.backend.backend.model;
+
+import javax.validation.constraints.NotBlank;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "category")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "category_name")
+    private @NotBlank String categoryName;
+
+    private @NotBlank String description;
+
+    @Column(name = "image_url")
+    private @NotBlank String imageUrl;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
