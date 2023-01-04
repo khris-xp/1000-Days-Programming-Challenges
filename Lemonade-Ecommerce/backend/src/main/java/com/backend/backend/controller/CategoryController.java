c213757bc4adc9fd443ca11d569f85b87c3612ee
Khris Bharmmano <84142253+khris-cpu@users.noreply.github.com>
2023-01-04 12:45:02 +0700

    add : category controller

--- Lemonade-Ecommerce/backend/src/main/java/com/backend/backend/controller/CategoryController.java
+++ Lemonade-Ecommerce/backend/src/main/java/com/backend/backend/controller/CategoryController.java
@@ -1,8 +1,24 @@
 package com.backend.backend.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.backend.backend.model.Category;
+import com.backend.backend.service.CategoryService;
+
 @RestController
+@RequestMapping("/category")
 public class CategoryController {
 
+    @Autowired
+    CategoryService categoryService;
+
+    @PostMapping("/create")
+    public String createCategory(@RequestBody Category category) {
+        categoryService.createCategory(category);
+        return "success";
+    }
 }
