332f3929aa3d4ac863bcc6c5153bbf6eb2e68a85
Khris Bharmmano <84142253+khris-cpu@users.noreply.github.com>
2023-01-05 16:06:35 +0700

    add : getter method

--- Java-OOP-Tutorial/Employee.java
+++ Java-OOP-Tutorial/Employee.java
@@ -6,11 +6,32 @@ class Employee {
 
     public void setId(String id) {
         this.id = id;
-        System.out.println("ID : " + id);
     }
 
     public void setName(String name) {
         this.name = name;
-        System.out.println("Name : " + name);
     }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    public void displayEmployee() {
+        System.out.println("ID : " + this.id);
+        System.out.println("Name : " + this.name);
+        System.out.println("Salary : " + this.salary);
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public Double getSalary() {
+        return this.salary;
+    }
+
 }
--- Java-OOP-Tutorial/Main.java
+++ Java-OOP-Tutorial/Main.java
@@ -1,9 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        Employee e1 = new Employee(); // Create Object
-        
+        // Create Object
+        Employee e1 = new Employee();
         e1.setId("12345");
         e1.setName("Khris Bharmmano");
+        e1.setSalary(30000.0);
+        System.out.println(e1.getName());
 
+        // Create Object
+        Employee e2 = new Employee();
+        e2.setId("67890");
+        e2.setName("John Doe");
+        e2.setSalary(20000.0);
+        System.out.println(e2.getSalary());
     }
 }
