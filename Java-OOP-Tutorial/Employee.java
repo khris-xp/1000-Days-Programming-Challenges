e2cff0105cda057377fb7049ece2032b463e2508
Khris Bharmmano <84142253+khris-cpu@users.noreply.github.com>
2023-01-05 13:50:00 +0700

    add : assign attribute and method

--- Java-OOP-Tutorial/Employee.java
+++ Java-OOP-Tutorial/Employee.java
@@ -3,4 +3,14 @@ class Employee {
     private String id;
     private String name;
     private Double salary;
+
+    public void setId(String id) {
+        this.id = id;
+        System.out.println("ID : " + id);
+    }
+
+    public void setName(String name) {
+        this.name = name;
+        System.out.println("Name : " + name);
+    }
 }
--- Java-OOP-Tutorial/Main.java
+++ Java-OOP-Tutorial/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         Employee e1 = new Employee(); // Create Object
+        
+        e1.setId("12345");
+        e1.setName("Khris Bharmmano");
+
     }
 }
