1391ca35ae5b6ff7dbeafaa113e57fa2444180cb
Khris Bharmmano <84142253+khris-cpu@users.noreply.github.com>
2023-01-07 12:53:28 +0700

    add : creating the application with vue cli

---
+++ basic-vue/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <h1>{{title}}</h1>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      title : 'My To-Do App'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
