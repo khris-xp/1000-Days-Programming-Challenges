7dff99b7b3ee756cd905922f6e826e23e05b3122
Khris Bharmmano <84142253+khris-cpu@users.noreply.github.com>
2023-01-07 16:27:40 +0700

    add : conditional rendering

--- basic-vue/src/App.vue
+++ basic-vue/src/App.vue
@@ -3,6 +3,16 @@
   <ul>
     <li v-for="task in tasks" :key="task.id">
       {{task.id}} . {{task.name}}
+
+      <div v-if="task.finished">
+        <button>Delete Task</button>
+      </div>
+      <div v-else-if="task.edit">
+        <button>Edit Task</button>
+      </div>
+      <div v-else>
+        <p>No Button</p>
+      </div>
     </li>
   </ul>
 </template>
@@ -14,7 +24,7 @@ export default {
     return {
       title : 'My To-Do App',
       tasks : [
-        {id : 1 , name : "Coding" , finished : false},
+        {id : 1 , name : "Coding" , finished : true},
         {id : 2, name : "Do a VUE JS" , finished: false},
         {id : 3, name : "Do a To Do List App" , finished : false}
       ]
