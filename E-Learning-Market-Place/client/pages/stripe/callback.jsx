ea171b559cdb5d82dfb3fc77810ebb44a1f61d9c
Khris Bharmmano <84142253+khris-cpu@users.noreply.github.com>
2023-01-09 15:54:16 +0700

    add : stripe callback page

---
+++ E-Learning-Market-Place/client/pages/stripe/callback.jsx
@@ -0,0 +1,26 @@
+import { useContext, useEffect } from "react";
+import { Context } from "../../context/index";
+import axios from "axios";
+import { Spin } from "antd";
+
+const stripeCallback = () => {
+  const {
+    state: { user },
+  } = useContext(Context);
+
+  useEffect(() => {
+    if (user) {
+      axios.post("/api/get-account-status").then((res) => {
+        window.location.href("/instructor");
+      });
+    }
+  }, [user]);
+
+  return (
+    <Spin tip="Loading" size="large">
+      <div className="content p-5" />
+    </Spin>
+  );
+};
+
+export default stripeCallback;
--- E-Learning-Market-Place/server/controllers/instructor.js
+++ E-Learning-Market-Place/server/controllers/instructor.js
@@ -1,35 +1,33 @@
 const Users = require('../models/user');
 const stripe = require('stripe')(process.env.STRIPE_SECRET);
-const queryString = require('node:querystring');
+const querystring = require('node:querystring');
 
 const makeInstructor = async (req, res) => {
     try {
-        // 1. find user from db
         const user = await Users.findById(req.user._id).exec();
-        // 2. if user dont have stripe_account_id yet, then create new
+
         if (!user.stripe_account_id) {
-          const account = await stripe.accounts.create({ type: "express" });
-          // console.log('ACCOUNT => ', account.id)
-          user.stripe_account_id = account.id;
-          user.save();
+            const account = await stripe.accounts.create({ type: "express" });
+            user.stripe_account_id = account.id;
+            user.save();
         }
-        // 3. create account link based on account id (for frontend to complete onboarding)
+
         let accountLink = await stripe.accountLinks.create({
-          account: user.stripe_account_id,
-          refresh_url: process.env.STRIPE_REDIRECT_URL,
-          return_url: process.env.STRIPE_REDIRECT_URL,
-          type: "account_onboarding",
-        });
-        //   console.log(accountLink)
-        // 4. pre-fill any info such as email (optional), then send url resposne to frontend
+            account: user.stripe_account_id,
+            refresh_url: process.env.STRIPE_REDIRECT_URL,
+            return_url: process.env.STRIPE_REDIRECT_URL,
+            type: "account_onboarding",
+          });
+
+        // console.log(accountLink);
         accountLink = Object.assign(accountLink, {
-          "stripe_user[email]": user.email,
+            "stripe_user[email]": user.email,
         });
-        // 5. then send the account link as response to fronend
-        res.send(`${accountLink.url}?${queryString.stringify(accountLink)}`);
-      } catch (err) {
-        console.log("MAKE INSTRUCTOR ERR ", err);
-      }
+
+        res.send(`${accountLink.url}?${querystring.stringify(accountLink)}`);
+    } catch (err) {
+        console.log("MAKE INSTRUCTOR ERROR ", err);
+    };
 };
 
 module.exports = makeInstructor;
