4177b02615e2afd734de35b3f51e04e69294bcb6
Khris Bharmmano <84142253+khris-cpu@users.noreply.github.com>
2023-01-13 15:30:43 +0700

    add : installation

---
+++ Hotel-Reservation/client/src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home/home";
+import List from "./list/List";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/hotels" element={<List />} />
+        <Route path="/hotels/:id" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
