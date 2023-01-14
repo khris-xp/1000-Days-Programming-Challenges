02a83585f2a17b5d77ebbbcaa6541a50c9e7dae0
Khris Bharmmano <84142253+khris-cpu@users.noreply.github.com>
2023-01-14 14:23:45 +0700

    add : set up

---
+++ Student-CRUD/server/src/server.ts
@@ -0,0 +1,28 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+const PORT: string | number = process.env.PORT || 8000;
+const Database: string = "mongodb://localhost:27017/student-db";
+
+// Connect to mongoDB
+mongoose.set("strictQuery", false);
+mongoose.connect(Database, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.on("connected", () => {
+  console.log("MongoDb has be connected.");
+});
+
+mongoose.connection.on("error", (err: Error) => {
+  console.log(`Mongoose connection error: ${err}`);
+});
+
+// Express 
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Server is running on PORT : ${PORT}`);
+});
