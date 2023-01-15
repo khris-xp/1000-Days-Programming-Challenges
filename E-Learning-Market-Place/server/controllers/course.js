9abe1d5a0b9d322d120050c3ee24d1277dd70562
Khris Bharmmano <84142253+khris-cpu@users.noreply.github.com>
2023-01-15 15:29:08 +0700

    add : upload image to s3

--- E-Learning-Market-Place/server/controllers/course.js
+++ E-Learning-Market-Place/server/controllers/course.js
@@ -1,5 +1,52 @@
+const AWS = require("aws-sdk");
+var { nanoid: ID, nanoid } = require("nanoid");
+
+const awsConfig = {
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION,
+  apiVersion: process.env.AWS_API_VERSION,
+};
+
+const S3 = new AWS.S3(awsConfig);
+
 const uploadImage = async (req, res) => {
-  console.log(req.body);
+  try {
+    const { image } = req.body;
+    if (!image) {
+      return res.status(400).send("No Image");
+    }
+
+    // Prepare the image
+    const base64Data = new Buffer.from(
+      image.replace(/^data:image\/\w+;base64,/, ""),
+      "base64"
+    );
+
+    const type = image.split(";")[0].split("/")[1];
+
+    // Image params
+    const params = {
+      Bucket: "e-learning-marketplace-bucket",
+      Key: `${nanoid()}.${type}`,
+      Body: base64Data,
+      ACL: "public-read",
+      ContentEncoding: "base64",
+      ContentType: `image/${type}`,
+    };
+
+    // Upload
+    S3.upload(params, (err, data) => {
+      if (err) {
+        console.log(err);
+        return res.sendStatus(400);
+      }
+      console.log(data);
+      res.send(data);
+    });
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 module.exports = { uploadImage };
