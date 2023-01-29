928d6be357de73fa7f2c887957f14788dc793f8e
Khris Bharmmano <84142253+khris-cpu@users.noreply.github.com>
2023-01-29 14:23:57 +0700

    add : display single course info

--- E-Learning-Market-Place/client/pages/course/[slug].jsx
+++ E-Learning-Market-Place/client/pages/course/[slug].jsx
@@ -1,15 +1,52 @@
 import axios from "axios";
 import { useRouter } from "next/router";
+import { Badge } from "antd";
+import { currencyFormatter } from "../../utils/helpers";
 
 const SingleCourse = ({ course }) => {
   const router = useRouter();
   const { slug } = router.query;
 
+  const {
+    name,
+    description,
+    instructor,
+    updatedAt,
+    lessons,
+    image,
+    price,
+    paid,
+    category,
+  } = course;
+
   return (
     <div>
-      <div className="container-fluid">
+      <div className="jumbotron bg-primary square">
         <div className="row">
-          <pre>{JSON.stringify(course, null, 4)}</pre>
+          <div className="col-md-8">
+            <h1 className="text-light font-weight-bold">{name}</h1>
+            <p className="lead">
+              {description && description.substring(0, 160)}...
+            </p>
+            <Badge
+              count={category}
+              style={{ backgroundColor: "#03a9f4" }}
+              className="pb-4 mr-2"
+            />
+            <p>Created By {instructor.name}</p>
+            <p suppressHydrationWarning>
+              Last updated {new Date(updatedAt).toLocaleDateString()}
+            </p>
+            <h4 className="text-light" suppressHydrationWarning>
+              {paid
+                ? `${currencyFormatter({ amount: price, currency: "usd" })}`
+                : "Free"}
+            </h4>
+          </div>
+          <div className="col-md-4">
+            <p>Show Course Image</p>
+            <p>Enroll</p>
+          </div>
         </div>
       </div>
     </div>
