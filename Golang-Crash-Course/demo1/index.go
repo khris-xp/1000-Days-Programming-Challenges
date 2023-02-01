421ebd7ee75aefdfa26ca2c17344df02e612a98f
Khris Bharmmano <84142253+khris-cpu@users.noreply.github.com>
2023-02-01 16:57:15 +0700

    add : variables

--- Golang-Crash-Course/demo1/index.go
+++ Golang-Crash-Course/demo1/index.go
@@ -5,7 +5,36 @@ import (
 )
 
 func main() {
-	//This is a comment
-	/* Multiple comment Line */
-	fmt.Println("Hello World!")
+	var student1 string = "John"
+	var student2 = "Jane"
+
+	fmt.Println(student1)
+	fmt.Println(student2)
+
+	exercise1()
+	exercise2()
+	exercise3()
+	exercise4()
+}
+
+func exercise1() {
+	var myNum int = 50
+	fmt.Println(myNum)
+}
+
+func exercise2() {
+	var myWord string = "Hello!"
+	fmt.Println(myWord)
+}
+
+func exercise3() {
+	var x = 5
+	var y = 10
+	var z = x + y
+	fmt.Println(z)
+}
+
+func exercise4() {
+	var x, y, z = 5, 10, 15
+	fmt.Println(x, y, z)
 }
