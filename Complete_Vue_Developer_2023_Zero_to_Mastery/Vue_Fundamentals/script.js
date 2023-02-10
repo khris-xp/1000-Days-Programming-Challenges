d46a1634e85d6fbd54342735122e0c3f8ec3866b
Khris Bharmmano <khrisbharmmano@gmail.com>
2023-02-10 12:18:06 +0700

    add : multiple the instance data

--- Complete_Vue_Developer_2023_Zero_to_Mastery/Vue_Fundamentals/script.js
+++ Complete_Vue_Developer_2023_Zero_to_Mastery/Vue_Fundamentals/script.js
@@ -3,6 +3,17 @@ Vue.createApp({
     return {
       firstName: "John",
       lastName: "Doe",
+      classApp: "App 1",
     };
   },
-}).mount("#app");
+}).mount(".app");
+
+// Vue.createApp({
+//   data() {
+//     return {
+//       firstName: "John",
+//       lastName: "Doe",
+//       classApp: "App 2",
+//     };
+//   },
+// }).mount(".app2");
