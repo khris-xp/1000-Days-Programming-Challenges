ed018472f5a2fe93c20ae34e67a6692678b3d22b
Khris Bharmmano <khrisbharmmano@gmail.com>
2023-02-15 15:44:31 +0700

    add : listenning to events

--- Complete_Vue_Developer_2023_Zero_to_Mastery/Vue_Fundamentals/script.js
+++ Complete_Vue_Developer_2023_Zero_to_Mastery/Vue_Fundamentals/script.js
@@ -6,11 +6,19 @@ const vm = Vue.createApp({
       classApp: 'App 1',
       url: 'http://example.com/',
       raw_url: '<p><a href="http://example.com/">Example.com</a></p>',
+      age: 20,
     };
   },
   methods: {
     fullName() {
       return `${this.firstName} ${this.lastName.toUpperCase()}`;
+    },
+    increment() {
+      this.age++;
+    },
+    updateLastName(event) {
+      this.lastName = event.target.value;
+      console.log(this.lastName);
     },
   },
 }).mount('#app');
