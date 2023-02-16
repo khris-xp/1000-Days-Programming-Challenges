3b229f320809dd4208c40b7811aa6640ca293139
Khris Bharmmano <khrisbharmmano@gmail.com>
2023-02-16 15:24:28 +0700

    add : component

--- angular-fundamental/src/app/app.component.ts
+++ angular-fundamental/src/app/app.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular-fundamental';
+  name = 'Khris Bharmmano';
+
+  buttonClick() {
+    console.log(this.name);
+  }
+
+  changeName(name: string) {
+    this.name = name;
+  }
 }
