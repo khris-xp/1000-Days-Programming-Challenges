f05f4f5762e25752732b5363db23d121990d807c
Khris Bharmmano <khrisbharmmano@gmail.com>
2023-02-17 18:32:04 +0700

    add : ngif else template

--- angular-fundamental/src/app/action-bar/action-bar.component.ts
+++ angular-fundamental/src/app/action-bar/action-bar.component.ts
@@ -9,6 +9,7 @@ export class ActionBarComponent {
   @Input() step: number = 1;
   @Output() counterChange = new EventEmitter<number>();
   @Input() counter: number = 20;
+  @Input() inputDisplay: boolean = true;
 
   decrease(): void {
     if (this.counter - this.step > -1) {
