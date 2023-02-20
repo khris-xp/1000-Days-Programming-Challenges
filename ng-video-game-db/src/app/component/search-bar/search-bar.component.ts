45031bdc92e9f0903c9fcbb9782aa66549ae5042
Khris Bharmmano <khrisbharmmano@gmail.com>
2023-02-20 16:16:20 +0700

    add : search bar

--- ng-video-game-db/src/app/component/search-bar/search-bar.component.ts
+++ ng-video-game-db/src/app/component/search-bar/search-bar.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
-  styleUrls: ['./search-bar.component.scss']
+  styleUrls: ['./search-bar.component.scss'],
 })
 export class SearchBarComponent {
+  constructor(private router: Router) {}
 
+  onSubmit(form: NgForm) {
+    this.router.navigate(['search', form.value.search]);
+  }
 }
