c1bb2e73233ac2984034cfa2b3a6d2ec6fdd6966
Khris Bharmmano <khrisbharmmano@gmail.com>
2023-02-23 15:21:32 +0700

    add : create component

--- MEAN-Stack-CRUD/frontend/src/app/app.module.ts
+++ MEAN-Stack-CRUD/frontend/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddBookComponent } from './components/add-book/add-book.component';
+import { BookDetailComponent } from './components/book-detail/book-detail.component';
+import { BookListComponent } from './components/book-list/book-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddBookComponent,
+    BookDetailComponent,
+    BookListComponent
   ],
   imports: [
     BrowserModule,
