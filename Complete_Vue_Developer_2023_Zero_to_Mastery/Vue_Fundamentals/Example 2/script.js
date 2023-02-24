27cf1125cc8a37f8604a046143e1b249ac45d6bf
Khris Bharmmano <khrisbharmmano@gmail.com>
2023-02-24 12:48:49 +0700

    add : binding styles

--- Complete_Vue_Developer_2023_Zero_to_Mastery/Vue_Fundamentals/Example 2/script.js
+++ Complete_Vue_Developer_2023_Zero_to_Mastery/Vue_Fundamentals/Example 2/script.js
@@ -3,11 +3,25 @@ let vm = Vue.createApp({
     return {
       isPurple: false,
       selectedClass: '',
+      size: 150,
     };
   },
   computed: {
     circle_classes() {
       return { purple: this.isPurple };
+    },
+    style_classes() {
+      return {
+        width: `${this.size}px`,
+        height: `${this.size}px`,
+        lineHeight: `${this.size}px`,
+      };
+    },
+    transform_classes() {
+      console.log('Trasform Classes');
+      return {
+        transform: 'rotate(30deg)',
+      };
     },
   },
 }).mount('#app');
